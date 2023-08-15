c9dfa1be5b0d39d5cf60ac44c3d9748c1a627f72
Hanna Krautstorfer <hanna@Hannas-Laptop.fritz.box>
2023-08-15 14:54:36 +0200

    added more blobs to blob page

--- lib/5.1.1-1.2/screens/blob_package_exercise.dart
+++ lib/5.1.1-1.2/screens/blob_package_exercise.dart
@@ -18,24 +18,48 @@ class BlobPackageExercise extends StatelessWidget {
           children: [
             Blob.animatedRandom(
               size: 400,
-              duration: const Duration(milliseconds: 1000),
-              styles: BlobStyles(color: const Color.fromARGB(21, 255, 193, 7)),
+              duration: const Duration(milliseconds: 2000),
               loop: true,
+              styles: BlobStyles(
+                  color: const Color.fromARGB(105, 155, 39, 176),
+                  fillType: BlobFillType.stroke),
               child: Center(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Quote:',
-                    style: kHeader1,
+                child: Blob.animatedRandom(
+                  size: 400,
+                  duration: const Duration(milliseconds: 1500),
+                  loop: true,
+                  styles: BlobStyles(
+                      color: const Color.fromARGB(105, 155, 39, 176),
+                      fillType: BlobFillType.stroke),
+                  child: Center(
+                    child: Blob.animatedRandom(
+                      size: 300,
+                      duration: const Duration(milliseconds: 1000),
+                      styles: BlobStyles(
+                          gradient: LinearGradient(colors: [
+                        Colors.purple.withOpacity(0.4),
+                        Colors.cyan.withOpacity(0.4),
+                      ]).createShader(const Rect.fromLTRB(0, 0, 300, 300))),
+                      loop: true,
+                      child: Center(
+                          child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Quote:',
+                            style: kHeader1,
+                          ),
+                          kPlaceholder8,
+                          const Text(
+                            '"blobbies are sexy"',
+                            style: TextStyle(fontStyle: FontStyle.italic),
+                          )
+                        ],
+                      )),
+                    ),
                   ),
-                  kPlaceholder8,
-                  const Text(
-                    '"blobbies are sexy"',
-                    style: TextStyle(fontStyle: FontStyle.italic),
-                  )
-                ],
-              )),
+                ),
+              ),
             )
           ],
         ),
