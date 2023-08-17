f29dbdbb04cc690a6fca25a9c7efbfce89c17351
Hanna Krautstorfer <hanna@Hannas-Laptop.fritz.box>
2023-08-17 17:04:45 +0200

    box decoration exercise added

--- lib/5.1.3/screens/box_decoration.dart
+++ lib/5.1.3/screens/box_decoration.dart
@@ -11,38 +11,38 @@ class BoxDecoExercise extends StatelessWidget {
         backgroundColor: Theme.of(context).primaryColor,
         title: const Text('Box Decoration'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              height: 200,
-              width: 200,
-              decoration: const BoxDecoration(
-                color: Colors.teal,
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromARGB(98, 0, 150, 135),
-                    blurRadius: 20,
-                    offset: Offset(10, 10),
-                  ),
-                ],
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(
+          4,
+          (index) => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 200,
+                width: 200,
+                decoration: const BoxDecoration(
+                  color: Colors.teal,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromARGB(98, 0, 150, 135),
+                      blurRadius: 20,
+                      offset: Offset(10, 10),
+                    ),
+                  ],
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                      bottomLeft: Radius.circular(20)),
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.start, // Align text to the start
-                  children: [
-                    Flexible(
-                      child: Center(
-                        child: AspectRatio(
-                          aspectRatio:
-                              1.0, // Maintain original aspect ratio of the image
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        child: Center(
                           child: Container(
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(20),
@@ -50,28 +50,26 @@ class BoxDecoExercise extends StatelessWidget {
                                 image: NetworkImage(
                                   'https://images.unsplash.com/photo-1457089328109-e5d9bd499191?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=663&q=80',
                                 ),
-                                fit: BoxFit
-                                    .cover, // Fit the image inside the container
+                                fit: BoxFit.cover,
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                        height: 8), // Add some space between the image and text
-                    Center(
-                      child: Text(
-                        'Hello World',
-                        style: kHeader1,
+                      const SizedBox(height: 8),
+                      Center(
+                        child: Text(
+                          'Hello World',
+                          style: kHeader1.copyWith(color: Colors.white),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ),
-          )
-        ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/chapter_list.dart
+++ lib/chapter_list.dart
@@ -30,7 +30,7 @@ final List<Chapter> chapters = [
       Exercise(name: 'Assets / Fonts', route: '/font_exercise'),
       Exercise(
           name: 'Light- / Darkmode / Switch', route: '/light_dark_exercise'),
-      Exercise(name: 'Box Decoration', route: '/box_decoration'),
+      Exercise(name: 'Box Decoration / Grid View', route: '/box_decoration'),
     ],
   )
 ];
