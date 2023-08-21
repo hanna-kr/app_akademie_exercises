5b6ef4c41bd48885736bef13015c3d196aaf1b85
Hanna Krautstorfer <hanna@Hannas-Laptop.local>
2023-08-21 16:56:12 +0200

    file image added

--- lib/5.1.3-3.1/screens/image_exercise.dart
+++ lib/5.1.3-3.1/screens/image_exercise.dart
@@ -3,6 +3,7 @@ import 'package:app_akademie_exercises/styles/spacing.dart';
 import 'package:app_akademie_exercises/styles/textstyles.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class ImageExercise extends StatelessWidget {
   const ImageExercise({super.key});
@@ -48,7 +49,14 @@ class ImageExercise extends StatelessWidget {
                           'https://images.unsplash.com/photo-1542878909-b444d587c249?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'),
                   title: 'Drakensberg',
                   subtitle:
-                      'The Drakensberg Mountains form a dramatic natural border between Lesotho and South Africa. ')
+                      'The Drakensberg Mountains form a dramatic natural border between Lesotho and South Africa. '),
+              CardContents(
+                  image: Image(
+                      image: FileImage(
+                          File('/Users/hanna/Development/tsitsikamma.jpeg'))),
+                  title: 'Tsitsikamma National Park',
+                  subtitle:
+                      'Tsitsikamma National Park is a multi-dimensional destination with dramatic coastal scenery, reefs, rivers, lush forest and delicate Fynbos.')
             ])
           ],
         ),
--- lib/5.1.3-3.1/widgets/card_images.dart
+++ lib/5.1.3-3.1/widgets/card_images.dart
@@ -9,16 +9,23 @@ class CardContents {
   CardContents({required this.image, required this.title, this.subtitle});
 }
 
-class ImageCard extends StatelessWidget {
+class ImageCard extends StatefulWidget {
   final List<CardContents> adventures;
 
   const ImageCard({super.key, required this.adventures});
 
   @override
+  State<ImageCard> createState() => _ImageCardState();
+}
+
+bool favIsClicked = false;
+
+class _ImageCardState extends State<ImageCard> {
+  @override
   Widget build(BuildContext context) {
     return Expanded(
       child: ListView.builder(
-        itemCount: adventures.length,
+        itemCount: widget.adventures.length,
         itemBuilder: (BuildContext context, int index) {
           return Padding(
             padding: const EdgeInsets.symmetric(vertical: 8),
@@ -27,32 +34,39 @@ class ImageCard extends StatelessWidget {
                   borderRadius: BorderRadius.circular(20)),
               child: Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                      ),
-                      child: adventures[index].image,
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
                     ),
+                    child: widget.adventures[index].image,
                   ),
                   ListTile(
+                    minLeadingWidth: 8,
                     title: Text(
-                      adventures[index].title,
+                      widget.adventures[index].title,
                       style: kHeader2,
                       overflow: TextOverflow.ellipsis,
                       maxLines: 2,
                     ),
                     subtitle: Text(
-                      adventures[index].subtitle ?? '',
+                      widget.adventures[index].subtitle ?? '',
                       overflow: TextOverflow.ellipsis,
                       maxLines: 2,
                     ),
                     trailing: IconButton(
                         alignment: Alignment.centerRight,
-                        icon: const Icon(Icons.favorite),
-                        onPressed: () {}),
+                        icon: Icon(
+                          Icons.favorite,
+                          color: favIsClicked
+                              ? const Color.fromARGB(255, 202, 12, 75)
+                              : null,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            favIsClicked = !favIsClicked;
+                          });
+                        }),
                   ),
                 ],
               ),
