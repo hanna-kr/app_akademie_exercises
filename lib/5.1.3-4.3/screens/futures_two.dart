c86e1a3aeba67fe97b0f06e4d08ddbd84f4554e1
Hanna Krautstorfer <hanna@Hannas-Laptop.fritz.box>
2023-09-12 16:56:49 +0200

    future 2 exercise added

--- lib/5.1.3-4.3/screens/futures_two.dart
+++ lib/5.1.3-4.3/screens/futures_two.dart
@@ -10,18 +10,41 @@ class FutureExercise2 extends StatefulWidget {
 
 class _FutureExercise2State extends State<FutureExercise2> {
   String text = '';
+  List<String> titles = [];
+  List<Widget> images = [];
 
-  Future<void> randomFuture() {
-    return Future.delayed(const Duration(seconds: 2), () {
-      setState(() {
-        text = 'Hello, Future';
-      });
+  Future<void> randomFuture() async {
+    await Future.delayed(const Duration(seconds: 2));
+    setState(() {
+      text = 'Hello, Future';
     });
   }
 
-  void randomFunction() async {
-    await randomFuture();
-    debugPrint('The result is: $text');
+  Future<void> getTitle() async {
+    await Future.delayed(const Duration(seconds: 3));
+    setState(() {
+      titles = [
+        'Harry Potter - Stein der Weisen',
+        'Harry Potter - Kammer des Schreckens',
+        'Harry Potter - Gefangene von Askaban',
+      ];
+    });
+  }
+
+  Future<void> getImages() async {
+    await Future.delayed(const Duration(seconds: 5));
+    setState(() {
+      images = [
+        Image.asset('assets/images/harry1.jpeg'),
+        Image.asset('assets/images/harry2.jpeg'),
+        Image.asset('assets/images/harry3.jpeg'),
+      ];
+    });
+  }
+
+  Future<void> getData() async {
+    await getTitle();
+    await getImages();
   }
 
   @override
@@ -36,15 +59,52 @@ class _FutureExercise2State extends State<FutureExercise2> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(
-                        Theme.of(context).primaryColor.withOpacity(0.7))),
-                onPressed: () {
-                  randomFunction();
-                },
-                child: const Text('Click me')),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  Theme.of(context).primaryColor.withOpacity(0.7),
+                ),
+              ),
+              onPressed: () {
+                randomFuture();
+              },
+              child: const Text('Aufgabe 1'),
+            ),
             kSpacing32,
             Text(text),
+            kSpacing32,
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  Theme.of(context).primaryColor.withOpacity(0.7),
+                ),
+              ),
+              onPressed: () {
+                getData();
+              },
+              child: const Text('Aufgabe 2'),
+            ),
+            kSpacing32,
+            Column(
+              children: titles.map((title) => Text(title)).toList(),
+            ),
+            kSpacing32,
+            Column(
+              children: images
+                  .map((image) => SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: image,
+                      ))
+                  .toList(),
+            ),
+            ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    Theme.of(context).primaryColor.withOpacity(0.7),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text('Aufgabe 3')),
           ],
         ),
       ),
